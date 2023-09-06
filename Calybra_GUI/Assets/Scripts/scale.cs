c22a308a459715e7e74668aca33697bc102e89bf
Waitwhoami <gamboldok@gmail.com>
2023-09-06 12:36:16 +0200

    Summary (required)

--- Calybra_GUI/Assets/Scripts/scale.cs
+++ Calybra_GUI/Assets/Scripts/scale.cs
@@ -2,30 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.XR;
 
 public class scale : MonoBehaviour
 {
-    [SerializeField] public int tCounter = 0;              //How many objects are touching the left plate
-    Vector2 pos;                                    //The original position of the left plate
-    Quaternion rot;                                 //The original orientation of the line on top
+    [SerializeField] public int tCounter = 0; //How many objects are touching the left or right plate
+    [SerializeField] public static int getThat2 = 3;        //1 - Both plates are touched    2 - One plate is touched    3 - Neither of the plates is touched
+    Vector2 lpos;                                    //The original position of the left plate
+    Vector2 rpos;
     [SerializeField] public GameObject holds;      //The line on top that rotates as the plate is held down
     [SerializeField] public GameObject lp;
     [SerializeField] public GameObject rp;
     // Start is called before the first frame update
     void Start()
     {
-        rot = transform.rotation;
-        pos = transform.position;
+        lpos = lp.transform.position;
+        rpos = rp.transform.position;
     }
 
     // Update is called once per frame              
     void Update()
-    {   
-        /*if ( ) {            //When there's no object touching the left plate, it gets reset to its
-            transform.position = pos;   //original position
-            holds.transform.rotation = rot;
-        }*/
-        Debug.Log("Peso registrato: " + (pos.y - transform.position.y) + "kg");
+    {
+        Debug.Log("tCounter: " + tCounter);
+        Debug.Log(gameObject.name + " plates touched: " + (getThat2 - 1));
+        if(tCounter > 0)
+        {
+            
+            getThat2--;
+            if (getThat2 < 1)
+            {
+                getThat2 = 1;
+            }
+
+
+        }
+        else if(tCounter == 0)
+        {
+            getThat2++;
+            if (getThat2 > 3)
+            {
+                getThat2 = 3;
+            }
+        }
+
+        if (getThat2 == 3) {            //When there's no object touching the left plate, it gets reset to its
+            //lp.transform.position = lpos;   //original position
+            //rp.transform.position = rpos;
+            
+        }
+        //Debug.Log("Peso registrato: " + (pos.y - lp.transform.position.y) + "kg");
         //The weight is calculated by subtracting the current y position of the left plate to its original one
     }
 
@@ -39,5 +64,21 @@ public class scale : MonoBehaviour
         tCounter--;
     }
 
-     
-}
+
+}/*
+  * getThat2++;
+
+            if (getThat2 < 3)
+            { 
+
+
+            else
+            {
+                getThat2--;
+                if (getThat2 < 1) { 
+                        getThat2 = 1; 
+                }
+            }
+
+            }
+  */ 
---
+++ Calybra_GUI/Assets/Scripts/tezt.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tezt : MonoBehaviour
+{
+
+    [SerializeField] uint damnRightImSomebody = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
